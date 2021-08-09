23bb3478e550daddc9acd5d4abee3d391203bea7
leducviet-wolf <leebeveto@gmail.com>
2021-08-09 22:18:02 +0700

    Add partner and faq crud

---
+++ eProject_SymphonyLimited/eProject_SymphonyLimited/Areas/Admin/Controllers/FaqController.cs
@@ -0,0 +1,92 @@
+﻿using eProject_SymphonyLimited.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eProject_SymphonyLimited.Areas.Admin.Controllers
+{
+    public class FaqController : Controller
+    {
+        SymphonyLimitedDBContext db = new SymphonyLimitedDBContext();
+
+        // GET: Admin/Faq
+        public ActionResult Index()
+        {
+            return View(db.Faq.AsEnumerable());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Faq f)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Faq.Add(f);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var faqById = db.Faq.FirstOrDefault(x => x.EntityId == id);
+            if (faqById != null)
+            {
+                return View(faqById);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Faq f)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(f).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (db.Faq.Find(id) != null)
+            {
+                try
+                {
+                    db.Faq.Remove(db.Faq.Find(id));
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ eProject_SymphonyLimited/eProject_SymphonyLimited/Areas/Admin/Controllers/PartnerController.cs
@@ -0,0 +1,92 @@
+﻿using eProject_SymphonyLimited.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eProject_SymphonyLimited.Areas.Admin.Controllers
+{
+    public class PartnerController : Controller
+    {
+        SymphonyLimitedDBContext db = new SymphonyLimitedDBContext();
+
+        // GET: Admin/Partner
+        public ActionResult Index()
+        {
+            return View(db.Partner.AsEnumerable());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Partner p)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Partner.Add(p);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var partnerById = db.Partner.FirstOrDefault(x => x.EntityId == id);
+            if (partnerById != null)
+            {
+                return View(partnerById);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Partner p)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(p).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (db.Partner.Find(id) != null)
+            {
+                try
+                {
+                    db.Partner.Remove(db.Partner.Find(id));
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return View();
+        }
+    }
+}
