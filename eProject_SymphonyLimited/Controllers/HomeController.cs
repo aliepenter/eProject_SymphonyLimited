857be847255afaf6bbcf78f5489136655d3a6f21
leducviet-wolf <leebeveto@gmail.com>
2021-08-15 22:31:51 +0700

    Fix category page and course page

--- eProject_SymphonyLimited/Controllers/HomeController.cs
+++ eProject_SymphonyLimited/Controllers/HomeController.cs
@@ -22,13 +22,20 @@ namespace eProject_SymphonyLimited.Controllers
             var teacher = db.Teacher.AsEnumerable();
             ViewBag.teacher = teacher;
         }
+
+        public ActionResult GetChildCategories()
+        {
+
+            return View();
+        }
+
         public ActionResult Index()
         {
             return View();
         }
+
         public ActionResult About()
         {
-            return View();
             return View();
         }
 
@@ -51,8 +58,60 @@ namespace eProject_SymphonyLimited.Controllers
                 data = childCategory
             }, JsonRequestBehavior.AllowGet);
         }
+
         public ActionResult Course()
         {
+            var id = RouteData.Values["id"];
+            if (id != null)
+            {
+                bool isInt = Int32.TryParse(id.ToString(), out int entityId);
+                var categoryById = db.Category.FirstOrDefault(x => x.EntityId == entityId);
+                if (categoryById != null)
+                {
+                    var childCategoriesById = db.Category.Where(x => x.ParentId == entityId).AsEnumerable();
+                    var allChildCategoriesById = db.Category.Where(x => x.Path.Contains("/" + entityId + "/")).AsEnumerable();
+                    var coursesByCatgoryId = db.Course.Where(x => x.CategoryId == entityId).ToList();
+                    if (coursesByCatgoryId.Count() > 0)
+                    {
+                        ViewBag.Coures = coursesByCatgoryId;
+                    }
+                    else
+                    {
+                        ViewBag.Coures = null;
+                    }
+                    if (childCategoriesById.Count() > 0)
+                    {
+                        ViewBag.ChildCategories = childCategoriesById;
+                    }
+                    else
+                    {
+                        ViewBag.ChildCategories = null;
+                    }
+                    if (allChildCategoriesById.Count() > 0)
+                    {
+                        foreach (var item in allChildCategoriesById)
+                        {
+                            var coursesInChildCategory = db.Course.Where(x => x.CategoryId == item.EntityId).AsEnumerable();
+                            if (coursesInChildCategory.Count() > 0)
+                            {
+                                foreach (var course in coursesInChildCategory)
+                                {
+                                    if (coursesByCatgoryId.FirstOrDefault(x => x.EntityId == course.EntityId) == null)
+                                    {
+                                        coursesByCatgoryId.Add(course);
+                                    }
+                                }
+                            }
+                        }
+                        ViewBag.ChildCategories = childCategoriesById;
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.Coures = db.Course.AsEnumerable();
+                ViewBag.ChildCategories = db.Category.Where(x => x.Level == 2).AsEnumerable();
+            }
             return View();
         }
         [HttpGet]
@@ -72,6 +131,7 @@ namespace eProject_SymphonyLimited.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult Application(Faq f)
         {
@@ -79,9 +139,21 @@ namespace eProject_SymphonyLimited.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
         public ActionResult CourseDetail()
         {
-            return View();
+            var id = RouteData.Values["id"];
+            if (id != null)
+            {
+                bool isInt = Int32.TryParse(id.ToString(), out int entityId);
+                var courseById = db.Course.FirstOrDefault(x => x.EntityId == entityId);
+                if (courseById != null)
+                {
+                    return View(courseById);
+                }
+            }
+            TempData["CoureNotExist"] = "This course is not exist!";
+            return RedirectToAction("Course");
         }
     }
 }
